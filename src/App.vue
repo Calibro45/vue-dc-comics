01c31a6b9b6ce324915141084fac1528f3b50751
andry16-10-91@hotmail.it <andry16-10-91@hotmail.it>
2022-03-08 15:14:00 +0100

    nav bar static

--- src/App.vue
+++ src/App.vue
@@ -22,5 +22,40 @@ export default {
 </script>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap');
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing:  border-box;
+}
+
+html {
+  font-size: 16px;
+  font-family: 'Roboto', sans-serif;
+}
+
+ol, ul {
+  list-style-type: none;
+}
+
+a {
+  text-decoration: none;
+  color: currentColor;
+}
+
+img {
+  max-width: 100%;
+  height: 100%;
+}
+
+.container-fluid {
+  width: 100%;
+}
+
+.container {
+  max-width: 1300px;
+  margin: 0 auto;
+}
 
 </style>
--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,6 +1,42 @@
 <template>
     <header>
-        navigazione
+        <div class="container nav-wrapper">
+            <figure class="logo">
+                <img src="../assets/img/dc-logo.png" alt="">
+            </figure>
+            <ol class="main-nav">
+                <li class="nav-link">
+                    <a href="#">character</a>
+                </li>
+                <li class="nav-link active">
+                    <a href="#">comics</a>
+                </li>
+                <li class="nav-link">
+                    <a href="#">movies</a>
+                </li>
+                <li class="nav-link">
+                    <a href="#">tv</a>
+                </li>
+                <li class="nav-link">
+                    <a href="#">games</a>
+                </li>
+                <li class="nav-link">
+                    <a href="#">collectibles</a>
+                </li>
+                <li class="nav-link">
+                    <a href="#">videos</a>
+                </li>
+                <li class="nav-link">
+                    <a href="#">fans</a>
+                </li>
+                <li class="nav-link">
+                    <a href="#">news</a>
+                </li>
+                <li class="nav-link">
+                    <a href="#">shop</a>
+                </li>
+            </ol>
+        </div>
     </header>
 </template>
 
@@ -12,5 +48,53 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
+.nav-wrapper {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 15px 5px;
+
+    .logo {
+        width: 85px;
+        height: 85px;
+        flex-shrink: 0;
+    }
+
+    .main-nav {
+        display: flex;
+        align-items: center;
+        gap: 30px;
+        flex-wrap: wrap;
+
+        .nav-link {
+            padding: 5px 10px;
+            text-transform: uppercase;
+            font-size: 14px;
+            font-weight: 700;
+            position: relative;
+
+            &.active {
+                color: #579cfa
+            }
+
+            &.active::after {
+                content: '';
+                display: block;
+                height: 5px;
+                background-color: #579cfa;
+                width: calc(100% - 20px);
+                position: absolute;
+                bottom: -100%;
+            }
+
+            &:hover {
+                cursor: pointer;
+                color: #579cfa;
+            }
+        }
+    }
+}
+
 
 </style>
