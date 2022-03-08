ef9fa6ea6f8c820654cdcbce4c73e7bef1a753de
andry16-10-91@hotmail.it <andry16-10-91@hotmail.it>
2022-03-08 17:47:57 +0100

    ciclo shop

--- src/components/MainSection.vue
+++ src/components/MainSection.vue
@@ -2,44 +2,14 @@
     <section>
         <div class="container">
         <ol class="shop-wrap">
-            <li class="shop-item">
+            <li class="shop-item" v-for="(item, i) in shopList" :key="i">
                 <figure class="shop-item-logo">
                     <img src="../assets/img/buy-comics-digital-comics.png" alt="">
                 </figure>
                 <div class="shop-item-title">
-                    <h4>digital comics</h4>
-                </div>
-            </li>
-            <li class="shop-item">
-                <figure class="shop-item-logo">
-                    <img src="../assets/img/buy-comics-merchandise.png" alt="">
-                </figure>
-                <div class="shop-item-title">
-                    <h4>digital comics</h4>
-                </div>
-            </li>
-            <li class="shop-item">
-                <figure class="shop-item-logo">
-                    <img src="../assets/img/buy-comics-subscriptions.png" alt="">
-                </figure>
-                <div class="shop-item-title">
-                    <h4>digital comics</h4>
-                </div>
-            </li>
-            <li class="shop-item">
-                <figure class="shop-item-logo">
-                    <img src="../assets/img/buy-comics-shop-locator.png" alt="">
-                </figure>
-                <div class="shop-item-title">
-                    <h4>digital comics</h4>
-                </div>
-            </li>
-            <li class="shop-item">
-                <figure class="shop-item-logo">
-                    <img src="../assets/img/buy-dc-power-visa.svg" alt="">
-                </figure>
-                <div class="shop-item-title">
-                    <h4>digital comics</h4>
+                    <a :href="item.ancora">
+                        <h4>{{ item.title }}</h4>
+                    </a>
                 </div>
             </li>
         </ol>
@@ -50,7 +20,38 @@
 <script>
 export default {
     name: 'mainSection',
-    
+
+    data() {
+        return {
+            shopList: [
+                {
+                    image: 'buy-comics-digital-comics.png',
+                    ancora: '#',
+                    title: 'digital comics'
+                },
+                {
+                    image: 'buy-comics-merchandise.png',
+                    ancora: '#',
+                    title: 'dc merchandise'
+                },
+                {
+                    image: 'buy-comics-subscriptions.png',
+                    ancora: '#',
+                    title: 'subscription'
+                },
+                {
+                    image: 'buy-comics-shop-locator.png',
+                    ancora: '#',
+                    title: 'comic shop locator'
+                },
+                {
+                    image: 'buy-dc-power-visa.svg',
+                    ancora: '#',
+                    title: 'dc power visa'
+                },
+            ],
+        }
+    }
 }
 </script>
 
@@ -65,27 +66,33 @@ section {
     .shop-wrap {
         display: flex;
         align-items: center;
-        gap: 2%;
+        gap: 1%;
         padding: 10px 40px;
 
         .shop-item {
             display: flex;
             align-items: center;
             flex-wrap: wrap;
-            width: calc(92% / 5);
-            gap: 20px;
+            width: calc(96% / 5);
 
             .shop-item-logo {
                 width: 70px;
                 height: 70px;
+
+                img {
+                    margin: 0 auto;
+                }
             }
 
             .shop-item-title {
                 text-transform: uppercase;
                 font-size: 14px;
+                flex-grow: 1;
+                white-space: nowrap;
 
                 h4 {
                     font-weight: 400;
+                    padding: 5px;
                 }
             }
         }
